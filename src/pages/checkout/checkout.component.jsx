d51bfbb91c2d6e775c2a67b5eb2de26818228193
Mayank <mayank.swarup@accenture.com>
2020-01-28 17:06:40 +0530

    updating stripe integration, favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import CheckoutItem from '../../components/checkout-item/checkout-item.component
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckOut = ({cartItems, total}) => (
     <div className="checkout-page">
@@ -31,7 +32,14 @@ const CheckOut = ({cartItems, total}) => (
         { cartItems.map(cartItem => <CheckoutItem cartItem={cartItem} key={cartItem.id} />)}
         <div className="total">
             <span> TOTAL: ${total}</span>
+           
         </div>
+        <div className="test-warning">
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+           </div>
+            <StripeCheckoutButton price={total} />
     </div>
 );
     const mapStateToProps = createStructuredSelector({
